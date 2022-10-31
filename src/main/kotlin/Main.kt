a66d1c85fd061344e7c8e226b955b3f371df0faa
Ilya Brant <detroitib31@gmail.com>
2022-10-31 17:35:08 +0600

    1 задача начало

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
-fun main(args: Array<String>) {
-    println("Hello Omsk!")
+import java.lang.Math.sqrt
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+
+    println("Введите x: ")
+    val x = reader.nextDouble()
+
+    println("Введите y: ")
+    val y = reader.nextDouble()
+
+    println("Введите r: ")
+    val r = reader.nextDouble()
+
+    val distance = sqrt(x*x + y*y)
+
+
+
 }
